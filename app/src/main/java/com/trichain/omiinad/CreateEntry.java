99d5f5f61a7c5b8578befa33c02039e4f01ec57a
karumajohn <fsh252203>
2020-02-13 04:29:55 +0300

    First Delivery

--- app/src/main/java/com/trichain/omiinad/CreateEntry.java
+++ app/src/main/java/com/trichain/omiinad/CreateEntry.java
@@ -139,7 +139,7 @@ public class CreateEntry extends AppCompatActivity implements OnMapReadyCallback
                 // Set up the input
                 final EditText input = new EditText(CreateEntry.this);
                 // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-                input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                input.setInputType(InputType.TYPE_CLASS_TEXT );
                 builder.setView(input);
 
                 // Set up the buttons
